27273bdfad1b85567491406f6289dc80e918cdf6
Matt Brody <mattlbrody@gmail.com>
2014-06-20 18:15:21 -0700

    original smarty files

---
+++ templates_c/3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7.file.footer.tpl.cache.php
@@ -0,0 +1,24 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2014-06-20 18:00:45
+         compiled from "./templates/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:59712604753a4d93d612233-83893942%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '3a4f6f0d327fc7bc3ea86f63906a1bf934ca50c7' => 
+    array (
+      0 => './templates/footer.tpl',
+      1 => 1396895534,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '59712604753a4d93d612233-83893942',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_53a4d93d614c88_62808902',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53a4d93d614c88_62808902')) {function content_53a4d93d614c88_62808902($_smarty_tpl) {?></BODY>
+</HTML>
+<?php }} ?>
---
+++ templates_c/97c13ae6868bbc459509c9f1b968154acd23eecc.file.header.tpl.cache.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.18, created on 2014-06-20 18:00:45
+         compiled from "./templates/header.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:38607332353a4d93d5fa251-57984755%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '97c13ae6868bbc459509c9f1b968154acd23eecc' => 
+    array (
+      0 => './templates/header.tpl',
+      1 => 1396895534,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '38607332353a4d93d5fa251-57984755',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'title' => 0,
+    'Name' => 1,
+  ),
+  'has_nocache_code' => true,
+  'version' => 'Smarty-3.1.18',
+  'unifunc' => 'content_53a4d93d601fe7_89030787',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_53a4d93d601fe7_89030787')) {function content_53a4d93d601fe7_89030787($_smarty_tpl) {?><HTML>
+<HEAD>
+<TITLE><?php echo $_smarty_tpl->tpl_vars['title']->value;?>
+ - <?php echo '/*%%SmartyNocache:38607332353a4d93d5fa251-57984755%%*/<?php echo $_smarty_tpl->tpl_vars[\'Name\']->value;?>
+/*/%%SmartyNocache:38607332353a4d93d5fa251-57984755%%*/';?>
+</TITLE>
+</HEAD>
+<BODY bgcolor="#ffffff">
+<?php }} ?>
